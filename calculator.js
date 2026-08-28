9de04bc4ff710aa67f0cb8b77fc492f870f8d032
Arcane <291515644+Arcane7pb@users.noreply.github.com>
2026-08-28 02:05:41 +0200

    Add extras

--- calculator.js
+++ calculator.js
@@ -9,6 +9,9 @@ const divide = (x, y) => +x / +y;
 let firstNum = "";
 let secondNum = "";
 let operator = "";
+let calc = 0;
+let firstDecimal = 0;
+let secondDecimal = 0;
 
 function operate (x, y, operat) {
     
@@ -16,14 +19,21 @@ function operate (x, y, operat) {
 
     total = (operat === "+") ? add(x, y) :
         (operat === "-") ? substract(x, y) :
+        (operat === "x") ? multiply(x, y) :
+        (operat === "%") ? divide(x, y) :
         (operat === "*") ? multiply(x, y) :
         (operat === "/") ? divide(x, y) :
         "Invalid Operator"
     ;
 
+    if ( (total % 1) != 0){
+        total = total.toFixed(4).replace(/0+$/, "")
+    }
+
     operator = "";
     firstNum = `${total}`;    
     secondNum = "";
+    calc += 1
 
     return total
 }
@@ -34,11 +44,11 @@ const display = document.querySelector(".displayOutput")
 
 function updateDisplay(){
 
-    display.textContent = `${firstNum} ${operator} ${secondNum}`;
+    display.textContent = `${firstNum}${operator}${secondNum}`;
 
 }
 
-//MANAGING INPUTS
+//MANAGING BUTTON INPUTS
 
 const numericalInputs = [
     {id : "zero", number: "0"},
@@ -59,19 +69,40 @@ const operatorInputs = [
     {id : "substract", number: "-"},
     {id : "product", number: "x"},
     {id : "divide", number: "%"},
+    {id : "productK", number: "*"},
+    {id : "divideK", number: "/"},
 ];
 
 function storeNumericalInput (input) {
     if(operator === ""){
-    firstNum += input;
+        if (calc != 0){
+            console.log("Number after operating")
+            reset()
+        }
+        if (input === "."){
+            if (firstDecimal != 0){
+                return
+            }
+            firstDecimal = 1;
+        }
+        firstNum += input;
     } else {
-    secondNum += input;
+        if (input === "."){
+            if (secondDecimal != 0){
+                return
+            }
+            secondDecimal = 1;
+        }
+        secondNum += input;
     }
 }
 
 function storeOperatorInput (input) {
     if(operator === ""){
         operator = input;
+    } else if (secondNum != ""){
+        operate(firstNum,secondNum,operator);
+        operator = input
     }
 }
 
@@ -79,9 +110,27 @@ function reset () {
     firstNum = "";
     secondNum = "";
     operator = "";
+    calc = 0;
+    firstDecimal = 0;
+    secondDecimal = 0;
 }
 
-function classifyInput (event) {
+function eraseLastNum () {
+    if (secondNum !== "") {
+        secondNum = secondNum.slice(0, -1);
+    } else if (operator === "") {
+        firstNum = firstNum.slice(0, -1);
+    }
+    updateDisplay()
+}
+
+function classifyButtonInput (event) {
+
+    if (event.key){
+        console.log("Key pressed");
+        console.log(event.key);
+        console.log(event.code);
+    }
 
     for (number of numericalInputs) {
         if (number.id === event.target.className) {
@@ -99,9 +148,12 @@ function classifyInput (event) {
         };
     };
 
+    if (event.target.className == "erase") {
+        eraseLastNum();
+    }
+
     if (event.target.className === "equal") {
             operate(firstNum, secondNum, operator);
-            updateDisplay();
             updateDisplay();
             return console.log("Solved!");
     };
@@ -116,4 +168,46 @@ function classifyInput (event) {
 
 const calculator = document.querySelector(".inputs");
 
-calculator.addEventListener("click", classifyInput )
+calculator.addEventListener("click", classifyButtonInput )
+
+//MANAGING KEYBOARD INPUTS with help of the previous button inputs
+
+function classifyKeyboardInput (event) {
+
+    for (number of numericalInputs) {
+        if (number.number === event.key) {
+            storeNumericalInput(number.number);
+            updateDisplay();
+            return console.log("Numerical input modified!");
+        };
+    };
+
+    for (operat of operatorInputs) {
+        if (operat.number === event.key) {
+            storeOperatorInput(operat.number);
+            updateDisplay();
+            return console.log("Operator input added!");
+        };
+    };
+
+    if ( (event.key == "Backspace") || (event.key == "delete") ) {
+        eraseLastNum();
+    }
+
+    if ( (event.key == "Enter") || (event.key == "=") ) {
+            operate(firstNum, secondNum, operator);
+            updateDisplay();
+            return console.log("Solved!");
+    };
+
+    if ( (event.key == "c") || (event.key == "C") ) {
+            reset();
+            updateDisplay();
+            return console.log("Reset!");
+    };
+
+};
+
+const keyboard = document.querySelector("body");
+
+keyboard.addEventListener("keydown", classifyKeyboardInput )
